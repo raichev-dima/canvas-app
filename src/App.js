4ce4e8ae024477d03efe4e323eb4121bb8b421cd
Dzmitry Raichau <d.raichau@gpsolutions.com>
2019-11-20 15:56:59 +0300

    feat(App): add ErrorBoundary to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 import { DataFileInput, Preview, DownloadButton } from './components';
+import ErrorBoundary from './ErrorBoundary';
+import styles from './App.module.scss';
 
 import './styles.scss';
-
-import styles from './App.module.scss';
 
 const FORM_ID = 'file-input-form';
 
 function App() {
   return (
-    <main>
-      <h1>Canvas App</h1>
-      <form id={FORM_ID} className={styles.inputForm}>
-        <DataFileInput />
-        <DownloadButton />
-        <Preview />
-      </form>
-    </main>
+    <ErrorBoundary>
+      <main>
+        <h1>Canvas App</h1>
+        <form id={FORM_ID} className={styles.inputForm}>
+          <DataFileInput />
+          <DownloadButton />
+          <Preview />
+        </form>
+      </main>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <h1 style={{ color: 'red' }}>
+          Something went wrong... Please, try refreshing the page
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export const ErrorThrower = ({ children }) => {
+  throw new Error(children);
+};
+
+export default ErrorBoundary;
